aeab3096b3fca2853146b204dc50b5c74714617f
D062706 <nicolas.knauber@sap.com>
2019-12-26 00:15:49 +0900

    add install_influx script and add proposal config

---
+++ utils/install_influxdb.sh
@@ -0,0 +1,17 @@
+#  Add the InfluxData repository to the file  /etc/apt/sources.list.d/influxdb.list
+echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+# Import apt key:
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+
+# Update apt index and install influxdb
+sudo apt-get update
+sudo apt-get install -y influxdb
+
+# Start and enable the service to start on boot up.
+
+sudo systemctl enable --now influxdb
+sudo systemctl is-enabled influxdb
+
+# Check service status:
+systemctl status influxdb
