a0a57aff01f9cf3c31d6592701b7bea83c35b10c
lobetdenherrn <nicolasknauber@gmx.de>
2020-02-24 10:25:33 +0100

    remove 'PRECISION' from create table sql

--- iotdb-benchmark/src/main/java/cn/edu/tsinghua/iotdb/benchmark/tsdb/timescaledb/TimescaleDB.java
+++ iotdb-benchmark/src/main/java/cn/edu/tsinghua/iotdb/benchmark/tsdb/timescaledb/TimescaleDB.java
@@ -387,7 +387,7 @@ public class TimescaleDB implements IDatabase {
     sqlBuilder.append("time BIGINT NOT NULL, sGroup TEXT NOT NULL, device TEXT NOT NULL");
     for (String sensor : sensors) {
       sqlBuilder.append(", ").append(sensor).append(" ").append(config.DATA_TYPE)
-          .append(" PRECISION NULL");
+          .append(" NULL");
     }
     sqlBuilder.append(");");
     return sqlBuilder.toString();
